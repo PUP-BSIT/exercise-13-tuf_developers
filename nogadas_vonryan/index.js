3e7f148a357803fd8552a66bc832e35ea369e92a
nogadas-vonryan <vonnogadas251@gmail.com>
2024-05-15 13:47:04 +0800

    change element get function to querySelectors instead of getByIds

--- nogadas_vonryan/index.js
+++ nogadas_vonryan/index.js
@@ -1,7 +1,7 @@
 function validateComment() {
-	let commentName = document.getElementById("comment_name");
-	let commentContent = document.getElementById("comment_content");
-	let commentButton = document.getElementById("comment_button");
+	let commentName = document.querySelector("#comment_name");
+	let commentContent = document.querySelector("#comment_content");
+	let commentButton = document.querySelector("#comment_button");
 
 	if (commentName.value && commentContent.value) {
 		commentButton.disabled = false;
