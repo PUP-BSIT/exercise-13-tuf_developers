70771fd516391dc967e038d2541ac1ce7a9d2aa2
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-05-15 19:34:24 +0800

    add previous comments

--- anonuevo_jheferson/script.js
+++ anonuevo_jheferson/script.js
@@ -1,6 +1,29 @@
 const commentName = document.querySelector("#name");
 const commentMessage = document.querySelector("#comment");
-
+const comments = [
+	{
+		name: "Von",
+		message:
+			"Your clarity of purpose in pursuing your goals is admirable",
+		date: new Date("2024-01-27"),
+	},
+	{
+		name: "Andrea",
+		message: "I hope you achieve your goal",
+		date: new Date("2024-02-27"),
+	},
+	{
+		name: "Mau",
+		message: "Persistence pays off, keep going",
+		date: new Date("2024-03-27"),
+	},
+	{
+		name: "Mark",
+		message: "You are making a difference",
+		date: new Date("2024-04-27"),
+	},
+];
+    
 function validateComment() {
 	if (commentName.value.length && commentMessage.value.length) {
 		commentButton.disabled = false;
