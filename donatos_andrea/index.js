f436422b92e04c3ffc6d7971c1a9f48fdaac621f
Andreadonatos <dreadonatos@gmail.com>
2024-05-15 18:42:30 +0800

    edit javascript file

--- donatos_andrea/index.js
+++ donatos_andrea/index.js
@@ -1,4 +1,4 @@
-function commentForm() {
+/*function commentForm() {
     let comment_button = document.getElementById("comment_button");
     let name = document.getElementById("member_name");
     let comment = document.getElementById("comment_area");
@@ -8,4 +8,87 @@ function commentForm() {
     } else {
         comment_button.disabled = true;
     }
-} 
+} */
+
+const memberName = document.querySelector("#member_name");
+const commentArea = document.querySelector("#comment_area");
+const commentButton = document.querySelector("#comment_button");
+
+const commentList = document.querySelector("#comment_list");
+const commentListData = [
+	{
+		name: "Jheferson",
+		comment: `Amazing goal, keep it up!`,
+		date: new Date("Sat Apr 27 22:28:24 2024")
+	},
+	{
+		name: "Von",
+		comment: `Your strategic approach to planning and
+        executing your life goals is impressive`,
+		date: new Date("Sat Apr 27 22:45:41 2024")
+	},
+	{
+		name: "Maui",
+		comment: `I love your goals, they reflect mine`,
+		date: new Date("Sat Apr 27 23:02:34 2024")
+	},
+	{
+		name: "Mark",
+		comment: `You could survive a zombie apocalypse`,
+		date: new Date("Sat Apr 27 23:06:32 2024")
+	}
+];
+
+function commentForm() {
+	 if (memberName.value.length > 0 && commentArea.value.length > 0) {
+         commentButton.disabled = false;
+     } else {
+         commentButton.disabled = true;
+}
+}
+
+function updateComments() {
+	commentList.innerHTML = "";
+
+	for (const comment of commentListData) {
+		 const newComment = document.createElement("li");
+		 const formatDate = new Date(comment.date);
+
+		newComment.innerHTML = `
+			<div class='upper-tags'>
+				<div class='user-tag'>${comment.name}</div>
+				<div class='date-tag'>${formatDate.toLocaleString()}</div>
+			</div>
+			<div class='user-comment'>${comment.comment}</div>`;
+
+		commentList.append(newComment);
+	}
+}
+
+function sortComments() {
+	const sortType = document.querySelector("#sort_type");
+	const newestSortType = "newest_first";
+
+	commentListData.sort((a, b) => {
+		if (sortType.value == newestSortType) {
+			return a.date - b.date;
+		} else {
+			return b.date - a.date;
+		}
+	});
+	
+	updateComments();
+}
+
+function addComment() {
+	const newComment = {
+		name: memberName.value,
+		comment: commentArea.value, 
+		date: new Date().toISOString(),
+	};
+
+	commentListData.push(newComment);
+	sortComments();
+}
+
+sortComments();
