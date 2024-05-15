eeb19088a8f9e555cc3c184fc4a86c165b126283
Andreadonatos <dreadonatos@gmail.com>
2024-05-15 20:24:24 +0800

    edit the button type

--- donatos_andrea/index.js
+++ donatos_andrea/index.js
@@ -1,15 +1,3 @@
-/*function commentForm() {
-    let comment_button = document.getElementById("comment_button");
-    let name = document.getElementById("member_name");
-    let comment = document.getElementById("comment_area");
-
-    if (name.value.length > 0 && comment.value.length > 0) {
-        comment_button.disabled = false;
-    } else {
-        comment_button.disabled = true;
-    }
-} */
-
 const memberName = document.querySelector("#member_name");
 const commentArea = document.querySelector("#comment_area");
 const commentButton = document.querySelector("#comment_button");
