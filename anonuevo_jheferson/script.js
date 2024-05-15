9f69dae9629b5747160d203e936f63ac700eff11
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-05-15 19:19:37 +0800

    refactor disable button

--- anonuevo_jheferson/script.js
+++ anonuevo_jheferson/script.js
@@ -1,15 +1,10 @@
+const commentName = document.querySelector("#name");
+const commentMessage = document.querySelector("#comment");
+
 function validateComment() {
-    let comment_name = document.getElementById("name");
-    let comment_message = document.getElementById("comment");
-    let comment_button = document.getElementById("comment_button");
-
-    if (comment_name.value.length < 1 ||
-        comment_message.value.length <1) {
-            comment_button.disabled = true;
-        } else {
-            comment_button.disabled = false;
-        }
+	if (commentName.value.length && commentMessage.value.length) {
+		commentButton.disabled = false;
+	} else {
+		commentButton.disabled = true;
+	}
 }
-
-document.getElementById("name").addEventListener("input", validateComment);
-document.getElementById("comment").addEventListener("input", validateComment);
